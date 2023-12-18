b737d5dd1dfc9bf00000dce60ac28286b2e05387
kristiafiliposki <filiposki.kristian1112@gmail.com>
2023-12-18 23:39:15 +0100

    first commit

---
+++ prova.php
@@ -0,0 +1,71 @@
+<?php 
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <?php 
+           $first = $hotels[0];
+            echo $first['name'];
+
+            $second = $hotels[1];
+            echo $second['name'];
+
+            $third = $hotels[2];
+            echo $third['name'];
+
+            $fourth = $hotels[3];
+            echo $fourth['name'];
+
+        ?>
+    </div>
+    <form action="prova2.php">
+        <input type="text" placeholder="voto" name="voto">
+        <button type="submit">send</button>
+    </form>
+</body>
+</html>
